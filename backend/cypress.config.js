e6cf1af95d5a279670a741d60c7bcedcd4bd9375
Ericton Brito <erictonbrito@outlook.com>
2025-03-02 23:07:19 -0300

    corrigindo cypress.config.js

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("Cypress");
+const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default
 
 module.exports = defineConfig({
